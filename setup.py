0d470544c5334504528a480c9fe30045609e0236
Lintang Adyuta Sutawika <lintang@konvergen.ai>
2018-06-29 19:56:51 +0700

    better installation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='pytorch-rbm',    # This is the name of your PyPI-package.
-    version='0.1',                          # Update the version number for new releases
-    scripts=['rbm.py']                  # The name of your scipt, and also the command you'll be using for calling it
+    name='pytorchrbm',
+    version='0.1',
+    packages=['pytorchrbm']
 )
